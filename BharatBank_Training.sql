6ea38e24b948c09ffd6c718dd43c257cbc449cb1
vivek-gohil <59043694+vivek-gohil@users.noreply.github.com>
2021-10-10 16:03:47 +0530

    Day 2

--- BharatBank_Training.sql
+++ BharatBank_Training.sql
@@ -493,9 +493,683 @@ SELECT
     *
 FROM 
     orders;
+    
+select count(*) from CONTACTS;
+select count(*) from COUNTRIES;
+select count(*) from CUSTOMERS;
+select count(*) from EMPLOYEES;
+select count(*) from INVENTORIES;
+select count(*) from LOCATIONS;
+select count(*) from ORDERS;
+select count(*) from ORDER_ITEMS;
+select count(*) from PRODUCT_CATEGORIES;
+select count(*) from PRODUCTS;
+select count(*) from REGIONS;
+select count(*) from WAREHOUSES;
+
+--Section 1. Querying Data
+DESC customers;
+
+SELECT
+    name
+FROM
+    customers;
+    
+SELECT 
+    customer_id,
+    name,
+    credit_limit
+FROM 
+    customers;
+    
+SELECT
+    *
+FROM
+    customers;
+    
+--Section 2. Sorting data
+SELECT 
+    customer_id,
+    name,
+    credit_limit
+FROM 
+    customers;
+    
+--Sort the customer data by names 
+SELECT 
+    customer_id,
+    name,
+    credit_limit
+FROM 
+    customers
+ORDER BY
+    name ASC;
+    
+SELECT 
+    customer_id,
+    name,
+    credit_limit
+FROM 
+    customers
+ORDER BY
+    UPPER(name) DESC;
+
+SELECT 
+    first_name,
+    last_name
+FROM
+    contacts;
+
+SELECT 
+    first_name,
+    last_name
+FROM
+    contacts
+ORDER BY
+    first_name ,last_name desc;
+    
+
+SELECT 
+    first_name,
+    last_name
+FROM
+    contacts
+ORDER BY
+    first_name ,last_name;    
+    
+    
+SELECT 
+    job_title, --1
+    first_name, --2
+    last_name --3
+FROM
+    employees
+ORDER BY
+    job_title,first_name;
+    
+    
+SELECT 
+    job_title, --1
+    first_name, --2
+    last_name --3
+FROM
+    employees
+ORDER BY
+    1,2 desc;
+    
+SELECT 
+    country_id,
+    city,
+    state
+FROM 
+    locations;
+    
+SELECT 
+    country_id,
+    city,
+    state
+FROM 
+    locations
+ORDER BY
+    state desc NULLS LAST;
+    
+    
+SELECT 
+    country_id,
+    city,
+    state
+FROM 
+    locations
+ORDER BY
+    state ASC NULLS FIRST;
+
+SELECT
+    order_id,
+    customer_id,
+    status,
+    order_date
+FROM
+    orders
+ORDER BY
+    order_date desc;
+
+--Section 3. Filtering data
+--DISTINCT
+SELECT 
+    first_name
+FROM
+    contacts
+ORDER BY
+    first_name;
+    
+SELECT 
+    DISTINCT first_name
+FROM
+    contacts
+ORDER BY
+    first_name;
+   
+SELECT
+    DISTINCT product_id,quantity
+FROM
+    order_items
+ORDER BY
+    product_id;
+    
+SELECT 
+    DISTINCT state
+FROM 
+    locations
+ORDER BY
+    state;   
+
+--Where
+SELECT
+    *
+FROM
+    products
+WHERE
+    product_name = 'Kingston';
+
+SELECT
+    *
+FROM
+    products 
+WHERE 
+    list_price > 500;
+    
+SELECT
+    *
+FROM
+    products 
+WHERE 
+    list_price > 500
+    AND
+    category_id = 4;
 
 
+--Between
+SELECT
+    *
+FROM
+    products 
+WHERE
+    list_price BETWEEN 650 AND 680
+ORDER BY
+    list_price;
 
+    
+SELECT
+    *
+FROM
+    products
+WHERE
+    category_id IN(1,4);
+    --category_id = 1
+    --OR
+    --category_id = 4;
+
+SELECT
+    *
+FROM
+    products
+WHERE
+    product_name like 'A%';
+
+--write a query returns the orders that belong to the customer id 44 and have canceled or pending status.
+--Use orders table
+
+SELECT  
+    *
+FROM
+    orders
+WHERE
+    status IN ('Canceled','Pending')
+    AND
+    customer_id = 44;
+
+--Get the order details placed in between 01-12-2016 and 31-12-2016
+SELECT  
+    *
+FROM
+    orders
+WHERE
+    order_date BETWEEN DATE '2016-12-01' AND DATE '2016-12-31'
+ORDER BY
+    order_date;
+    
+SELECT  
+    *
+FROM
+    orders;
+
+
+--all sales orders which do not have a no salesman:
+SELECT  
+    *
+FROM
+    orders
+WHERE
+    salesman_id IS NULL;
+
+SELECT  
+    *
+FROM
+    orders
+WHERE
+    salesman_id IS NOT NULL;
+    
+--Joins
+
+CREATE TABLE palette_a (
+    id NUMBER PRIMARY KEY,
+    color VARCHAR2(40) NOT NULL
+);
+
+CREATE TABLE palette_b (
+    id NUMBER PRIMARY KEY,
+    color VARCHAR2(40) NOT NULL
+);
+
+INSERT INTO palette_a VALUES (1,'Red');
+INSERT INTO palette_a VALUES (2,'Green');
+INSERT INTO palette_a VALUES (3,'Blue');
+INSERT INTO palette_a VALUES (4,'Purple');
+
+INSERT INTO palette_b VALUES (1,'Green');
+INSERT INTO palette_b VALUES (2,'Red');
+INSERT INTO palette_b VALUES (3,'Cyan');
+INSERT INTO palette_b VALUES (4,'Brown');
+
+SELECT * FROM palette_a;
+SELECT * FROM palette_b;
+
+--Oracle inner Join
+SELECT 
+    a.id id_a,
+    a.color color_a,
+    b.id id_b,
+    b.color color_b
+FROM
+    palette_a a,
+    palette_b b;
+-----------------------------
+SELECT 
+    a.id id_a,
+    a.color color_a,
+    b.id id_b,
+    b.color color_b
+FROM
+    palette_a a
+    INNER JOIN
+    palette_b b
+ON
+    a.color = b.color;
+    
+--Left outer join
+SELECT 
+    a.id id_a,
+    a.color color_a,
+    b.id id_b,
+    b.color color_b
+FROM
+    palette_a a LEFT JOIN palette_b b
+ON
+    a.color = b.color;
+    
+--Right outer join
+SELECT 
+    a.id id_a,
+    a.color color_a,
+    b.id id_b,
+    b.color color_b
+FROM
+    palette_a a RIGHT JOIN palette_b b
+ON
+    a.color = b.color;
+
+--full outer join
+SELECT 
+    a.id id_a,
+    a.color color_a,
+    b.id id_b,
+    b.color color_b
+FROM
+    palette_a a FULL JOIN palette_b b
+ON
+    a.color = b.color;
+    
+--orders and order_items
+SELECT * FROM orders;
+SELECT * FROM order_items;
+
+SELECT 
+    orders.order_id order_number,
+    order_items.unit_price price
+FROM
+    orders
+    INNER JOIN
+    order_items
+ON
+    order_items.order_id = orders.order_id
+WHERE 
+    order_items.order_id = 1;
+-------------------------------------------
+SELECT 
+    o.order_id order_number,
+    oi.unit_price price
+FROM
+    orders o
+    INNER JOIN
+    order_items oi
+ON
+    oi.order_id = o.order_id
+WHERE 
+    oi.order_id = 1;    
+    
+--USING CLAUSE IN JOIN
+SELECT 
+ *
+FROM
+    orders 
+    INNER JOIN
+    order_items
+USING(order_id);
+
+
+--Left Outer Join
+SELECT * FROM orders;
+SELECT * FROM employees;
+
+--Print the first name and last name of salesman all orders
+SELECT 
+    order_id,
+    status,
+    first_name salesman_first_name,
+    last_name salesman_last_name
+FROM
+    orders 
+    LEFT JOIN
+    employees
+ON
+    employee_id = salesman_id;
+    
+    
+SELECT * FROM orders;
+SELECT * FROM customers;
+
+--Print the name(customer name) ,orderid ,status , order date of customer
+SELECT
+    name,
+    order_id,
+    status,
+    order_date
+FROM
+    customers
+        LEFT JOIN
+    orders USING(customer_id);
+    
+
+
+--Print the first name and last name of salesman all orders
+SELECT 
+ 
+    first_name salesman_first_name,
+    last_name salesman_last_name,
+    order_id,
+    status
+FROM
+    orders 
+    RIGH JOIN
+    employees
+ON
+    employee_id = salesman_id;
+    
+--Oracle Full outer join
+CREATE TABLE projects (
+    project_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    project_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO projects(project_name) VALUES ('ERP');
+INSERT INTO projects(project_name) VALUES ('Sales CRM');
+
+SELECT  * FROM projects;
+
+CREATE TABLE members(
+    member_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    member_name VARCHAR(100) NOT NULL,
+    project_id NUMBER(4)
+);
+
+INSERT INTO members(member_name,project_id)
+VALUES('Vivek Gohil',1);
+
+INSERT INTO members(member_name,project_id)
+VALUES('Adarsh Poojary',2);
+
+INSERT INTO members(member_name,project_id)
+VALUES('Jack Daniel',NULL);
+
+SELECT * FROM members;
+
+--Full outer join
+SELECT
+    member_name,
+    project_name
+FROM
+    members m
+    FULL OUTER JOIN 
+    projects p
+ON
+    p.project_id = m.project_id;
+    
+--Self join
+SELECT * FROM employees;
+
+select 
+    (e.first_name || ' ' || e.last_name) employee,
+    (m.first_name || ' ' || m.last_name) manager,
+    e.job_title
+FROM
+    employees e
+    LEFT JOIN
+    employees m
+ON
+    m.employee_id = e.manager_id;
+
+--Section 5. Grouping data
+SELECT
+    *
+FROM
+    orders;
+    
+--Retrive the unique status
+SELECT
+    DISTINCT status
+FROM
+    orders;
+    
+--Count of orders for each status.
+SELECT 
+    status,
+    COUNT(order_id)
+FROM
+    orders
+WHERE
+    status = 'Shipped';
+    
+SELECT 
+    status,
+    COUNT(order_id)
+FROM
+    orders
+GROUP BY
+    status;
+    
+SELECT
+    job_title
+FROM
+    employees;
+    
+--Print the count of each job title 
+SELECT
+    job_title,
+    count(job_title)
+FROM
+    employees
+GROUP BY
+    job_title;
+    
+ 
+SELECT
+    *
+FROM
+    orders;
+
+SELECT 
+    EXTRACT(YEAR FROM SYSDATE) YEAR
+FROM
+    dual;
+    
+SELECT 
+    EXTRACT(MONTH FROM SYSDATE) MONTH
+FROM
+    dual;
+    
+--PRINT THE COUNT OF ORDERS PLACED PER YEAR
+
+SELECT
+    EXTRACT(YEAR FROM order_date) YEAR,
+    COUNT(order_id)
+FROM
+    orders
+GROUP BY
+    EXTRACT(YEAR FROM order_date)
+ORDER BY
+    YEAR;
+    
+SELECT
+    order_id,
+    SUM(unit_price * quantity) order_value
+FROM
+    order_items
+GROUP BY
+    order_id
+HAVING 
+     SUM(unit_price * quantity) > 1000000
+ORDER BY
+    order_value desc;
+
+--Find the orders whose values are greater than 500,000 
+--and the number of products in each order is between 10 and 12.
+
+SELECT 
+    order_id,
+    COUNT(item_id) item_count,
+    SUM(unit_price * quantity) order_value
+FROM
+    order_items
+GROUP BY
+    order_id
+HAVING
+    SUM(unit_price * quantity) > 500000
+    AND
+    COUNT(item_id) BETWEEN 10 AND 12
+ORDER BY
+    order_value DESC,
+    item_count DESC;
+    
+--Subquery
+
+SELECT
+    *
+FROM
+    products;
+    
+SELECT 
+    MAX(LIST_PRICE)
+FROM
+    products;
+    
+SELECT 
+    product_id,
+    product_name,
+    list_price
+FROM
+    products
+where 
+    list_price = 8867.99;
+
+
+--Using SubQuery
+SELECT 
+    product_id,
+    product_name,
+    list_price
+FROM
+    products
+where 
+    list_price = (
+        SELECT 
+            MAX(LIST_PRICE)
+        FROM
+        products
+    );
+
+SELECT 
+    product_id,
+    product_name,
+    list_price
+FROM
+    products
+where 
+    list_price = (
+        SELECT 
+            MIN(LIST_PRICE)
+        FROM
+        products
+    );
+
+--Exisits
+--Find all the customers who have the order
+SELECT
+    *
+FROM
+    customers;
+
+SELECT
+    *
+FROM
+    orders;
+    
+SELECT
+    name
+FROM
+    customers c
+WHERE
+    EXISTS (
+        SELECT
+            *
+        FROM
+            orders
+        WHERE
+            customer_id = c.customer_id
+    );
+    
+SELECT
+    name
+FROM
+    customers c
+WHERE
+    NOT EXISTS (
+        SELECT
+            *
+        FROM
+            orders
+        WHERE
+            customer_id = c.customer_id
+    );
 
 
 
